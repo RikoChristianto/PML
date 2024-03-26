0839c5e88a6261f3fb64043bfca4cbc86cb7b685
RikoChristianto <127176003+RikoChristianto@users.noreply.github.com>
2024-03-26 15:23:23 +0700

    Add files via upload

---
+++ coba.py
@@ -0,0 +1,32 @@
+print("Hello Python")
+print("riko")
+print('riko')
+
+print("Saya sudah sarapan tadi pagi\n dan nanti siang makan lagi")
+print("Saya sudah sarapan tadi pagi\t dan nanti siang makan lagi")
+
+print("Programming","Essentials","in")
+print("Python")
+
+print("Programming","Essentials","in", sep=" *** ", end=" :)\n")
+print("Python"*2)
+
+print("            *          \t"*4)
+print("            ***        \t"*4)
+print("*************   **     \t"*4)
+print("*                 **   \t"*4)
+print("*                   ** \t"*4)
+print("*                 **   \t"*4)
+print("*************   **     \t"*4)
+print("            ***        \t"*4)
+print("            *          \t"*4)
+
+print()
+
+print('"I\'m"\n""learning""\n"""Python"""')
+
+print("""
+"I'm"
+""learning""
+\"""Python""\"
+""")
